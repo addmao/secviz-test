8cda7f72ff4d57fb2cfe70db625c6dd9b9e94b8e
addmao <put_1996@live.com>
2018-08-12 19:33:06 +0700

    Finished. But the visualization seems strange

--- csv2_json_international.py
+++ csv2_json_international.py
@@ -13,17 +13,17 @@ def NotDuplicate(regex_pattern):
 		return False
 
 #Node
-with open('2018-07-Domestic Exchange - Index.csv') as domesticExchange:
+with open('2018-07-International Exchange - _Index.csv') as interExchange:
 	print "{"
 	print " \"nodes\": ["
 
-	for line in domesticExchange:
+	for line in interExchange:
 		regex_pattern = re.match(r'(.*),(.*),(.*),(.*),(.*),(.*),(.*)\n', line)
 		regex_end = re.match(r'(.*),(.*),(.*),(.*),(.*),(.*),(.*)\Z', line)
 
 
 		if regex_pattern:				
-			if NotDuplicate(regex_pattern.group(1)) and regex_pattern.group(1) != 'AS63529':
+			if NotDuplicate(regex_pattern.group(1)) and regex_pattern.group(1) != 'AS38794': #find needle...
 				print "  {"
 				print "    " + " \"color\": \"#" + str(hex(randint(0, 16777215)).split('x')[-1]) + "\","
 				print "    " + " \"label\": \"" + (regex_pattern.group(1)) + "\","
@@ -32,7 +32,7 @@ with open('2018-07-Domestic Exchange - Index.csv') as domesticExchange:
 				print "    " + " \"y\": " +  str(randint(1, 100)) + ","
 				print "    " + " \"x\": " +  str(randint(1, 100)) + ","
 				print "    " + " \"id\": \"" + (regex_pattern.group(1)) + "\","
-				print "    " + " \"size\": " + regex_pattern.group(5)
+				print "    " + " \"size\": " + str(int(regex_pattern.group(5)) / 10)
 				print "  },"
 
 		if regex_end:
@@ -49,11 +49,11 @@ with open('2018-07-Domestic Exchange - Index.csv') as domesticExchange:
 				print "  }"
 	print " ],"
 
-with open('2018-07-Domestic Exchange - Index.csv') as domesticExchange:
+with open('2018-07-International Exchange - _Index.csv') as interExchange:
 
 	print " \"edges\": ["
 
-	for line1 in domesticExchange:
+	for line1 in interExchange:
 		regex_pattern = re.match(r'(.*),(.*),(.*),(.*),(.*),(.*),(.*)\n', line1)
 		regex_end = re.match(r'(.*),(.*),(.*),(.*),(.*),(.*),(.*)\Z', line1)
 
